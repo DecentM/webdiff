7bdca682294d22a3fff1ad10ad4895de04e1dbcc
DecentM <DecentM@users.noreply.github.com>
2016-07-22 00:12:02 +0200

    Comparing and HTML hashing

--- webdiff.sh
+++ webdiff.sh
@@ -26,11 +26,12 @@ if [ "$1" = "clean" ]; then
 			tormname="$(echo $l | cut -d "#" -f2)"
 			tormhash="$(echo $l | cut -d "#" -f3)"
 			tormchash="$(echo $l | cut -d "#" -f4)"
+			tormhhash="$(echo $l | cut -d "#" -f5)"
 			if [ ! -d "$l" ]; then
 				printf "$l doesn't exist! Something fishy happened!\n"
 				exit 2
 			fi
-			printf "$tormname#$(date "$dateformat" -d @$tormdate)#$tormhash#$tormchash\n" >> "$qfile"
+			printf "$tormname#$(date "$dateformat" -d @$tormdate)#$tormhash#$tormchash#$tormhhash\n" >> "$qfile"
 			rm -rf "$l/"
 			if [ -d "www.$1" ]; then
 				rm -rf "www.$l/"
@@ -64,7 +65,8 @@ if [ "$1" = "list" ]; then
         	                tolsname="$(echo $l | cut -d "#" -f2)"
                 	        tolshash="$(echo $l | cut -d "#" -f3)"
 				tolschash="$(echo $l | cut -d "#" -f4)"
-                	        printf "$tolsname#$(date "$dateformat" -d @$tolsdate)#$tolshash#$tolschash\n" >> "$qfile"
+				tolshhash="$(echo $l | cut -d "#" -f5)"
+                	        printf "$tolsname#$(date "$dateformat" -d @$tolsdate)#$tolshash#$tolschash#$tolshhash\n" >> "$qfile"
                 	fi
         	done
 		cat "$qfile" | column -t -s "#"
@@ -73,23 +75,22 @@ if [ "$1" = "list" ]; then
 	exit 0
 fi
 
-printf "Webpage modificiation detector\n"
-
 curdate="$(date +""%s"")"
 curid="$(echo $1#$curdate | sha256sum | cut -d " " -f1)"
-printf "ID: $curid\n"
 
-printf "\nDownloading $1...\n\n"
+printf "Downloading $1...\n"
 wget -q -E -H -k -K -p "$1"
 if [ -d "$1" ]; then
-	printf "\nReorganizing files and directories...\n"
+	printf "Reorganizing files and directories...\n"
 	if [ -d "www.$1" ]; then
 		mv "www.$1" "$1"/
 	fi
 	contenthash="$(find -name $curdate#$1#$curid* -type f -exec cat {} \; | sha256sum | cut -d ' ' -f1)"
 	mv "$1" "$curdate#$1#$curid#$contenthash"
-	printf "\nDownloaded, writing to arbitrary flatfile database ($dbfile)\n"
-	printf "$curdate#$1#$curid#$contenthash\n" >> "$dbfile"
+	htmlhash="$(find $curdate#$1#$curid#$contenthash -name "*.html" -type f -exec cat {} \; | sha256sum | cut -d ' ' -f1)"
+	mv "$curdate#$1#$curid#$contenthash" "$curdate#$1#$curid#$contenthash#$htmlhash"
+	printf "Downloaded, writing to arbitrary flatfile database ($dbfile)\n"
+	printf "$curdate#$1#$curid#$contenthash#$htmlhash\n" >> "$dbfile"
 else
 	printf "Downloading $1 was unsuccessful, quitting...\n"
 	exit 1
@@ -109,8 +110,26 @@ do
 		cbeforedate="$cdate"
 	fi
 done
-printf "Newest copy of $1 is on this line: $(($(cat $dbfile | wc -l) - 1))"
 
+if [ "$(cat $dbfile | wc -l)" -gt "2" ]; then
+	newest_n="$(($(cat $dbfile | wc -l)))"
+	secondnewest_n="$(($(cat $dbfile | wc -l) - 1))"
+	newest_chash=$(head -n"$newest_n" $dbfile | tail -1 | cut -d "#" -f4)
+	secondnewest_chash=$(head -n"$secondnewest_n" $dbfile | tail -1 | cut -d "#" -f4)
+
+	if [ "$secondnewest_chash" != "" ] && [ "$newest_chash" != "" ]; then
+		if [ "$secondnewest_chash" = "$newest_chash" ]; then
+			printf "There has been no change since last time\n"
+		else
+			printf "The site has changed since last time\n"
+		fi
+	else
+		printf "There has been an internal error, quitting..."
+		exit 2
+	fi
+else
+	printf "We don't have enough copies of the site yet to compare\n"
+fi
 
 ## == End script == ##
 printf "\n"
